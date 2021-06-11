0455dbe67c7a20d12b615f4356476f1fc7302a92
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-06-11 16:28:39 +0430

    first test

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/home')
+def index():
+    return render_template("home.html", page='HOME')
+
+@app.route('/about')
+def about():
+    return render_template("about.html", page='ABOUT')
+
+@app.route('/menu')
+def menu():
+    return render_template("menu.html", page='MENU')
+
+@app.route('/where')
+def where():
+    return render_template("where.html", page='WHERE')
+
+
 
 
 if __name__ == '__main__':
